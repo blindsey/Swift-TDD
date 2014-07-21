6a268e3a990d14e60e6e197babb62fcf89df2382
Ben Lindsey <ben.lindsey@gmail.com>
2014-07-21 02:00:28 -0700

    added tester func as an extension to KIFTestCase

--- TDDTests/TDDTests.swift
+++ TDDTests/TDDTests.swift
@@ -9,7 +9,13 @@
 import UIKit
 import XCTest
 
-class TDDTests: SwiftTestCase {
+extension KIFTestCase {
+    func tester(_ file : String = __FILE__, _ line : Int = __LINE__) -> KIFUITestActor {
+        return KIFUITestActor(inFile: file, atLine: line, delegate: self)
+    }
+}
+
+class TDDTests: KIFTestCase {
     
     override func setUp() {
         super.setUp()
